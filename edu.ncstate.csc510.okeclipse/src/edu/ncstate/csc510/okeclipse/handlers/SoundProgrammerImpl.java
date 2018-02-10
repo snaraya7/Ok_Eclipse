ff4218d5eeaffb1593723de851fee251bd64d817
kmedidi <kmedidi@ncsu.edu>
2018-02-10 17:57:47 -0500

    SoundProgrammerImpl.generateGetterSetter() updated

--- edu.ncstate.csc510.okeclipse/src/edu/ncstate/csc510/okeclipse/handlers/SoundProgrammerImpl.java
+++ edu.ncstate.csc510.okeclipse/src/edu/ncstate/csc510/okeclipse/handlers/SoundProgrammerImpl.java
@@ -32,18 +32,6 @@ public class SoundProgrammerImpl implements ISoundProgrammer {
 		String getSetResult = javaSourceCode.substring(0,javaSourceCode.length()-1);
 		List<String> getSetResultLines = new ArrayList<String>();
 		
-// Finding indentation		
-		int flag = 0;
-		String JSC_findIndent= javaSourceCode.substring(javaSourceCode.indexOf('{')+1);
-		JSC_findIndent = JSC_findIndent.replaceAll("\r\n", "");
-		for(int i = 0; i < JSC_findIndent.length(); i++) {
-			if (Character.isLetter(JSC_findIndent.charAt(i))) {
-				flag = i;
-				break;
-			}
-		}
-		String indentation = JSC_findIndent.substring(0, flag);
-		
 // Extracting names and types of members		
 		String JSC_temp = javaSourceCode.replaceAll("public", "").replaceAll("private", "").replaceAll("\r\n", "");
 		JSC_temp = JSC_temp.substring(JSC_temp.indexOf("{")+1, JSC_temp.length()-1);
@@ -69,16 +57,17 @@ public class SoundProgrammerImpl implements ISoundProgrammer {
 			rem = "\\{" + rem + "\\}"; 
 			JSC_modified = JSC_modified.replaceFirst(rem, "");
 		}
+
 		List<String> JSC_modifiedList = new ArrayList<String>(Arrays.asList(JSC_modified.split("#")));
 		JSC_modified = "";
 		String item;
-		for(int i = 0; i < JSC_modifiedList.size()-1; i++) {
+		for(int i = 0; i < JSC_modifiedList.size(); i+=2) {
 			item = JSC_modifiedList.get(i+1);
-			if(item.contains("(")) {
-				JSC_modified += JSC_modifiedList.get(i) + "#";
+			if(!item.contains("(")) {
+				JSC_modified += JSC_modifiedList.get(i) + "#" + item + "#";
 			}
 			else {
-				i++;
+				i+=2;
 			}
 		}
 		JSC_modified = JSC_modified.replaceAll("\\s+", "").replaceAll(";", "");
@@ -88,12 +77,12 @@ public class SoundProgrammerImpl implements ISoundProgrammer {
 		for(int i = 0; i < JSC_memberList.size(); i+=2) {
 			String member_type = JSC_memberList.get(i);
 			String member_identifier = JSC_memberList.get(i+1);
-			String getter = "\r\n" + indentation + "public " + member_type + " get" + member_identifier + "()" + "{\r\n" +
-			indentation + "\treturn " + member_identifier + ";\r\n" + 
-			indentation + "}";
-			String setter = "\r\n" + indentation + "public " + "void" + " set" + member_identifier + "(" + member_type + " " + member_identifier + ")" + "{\r\n" + 
-			indentation + "\tthis." + member_identifier + " = " + member_identifier +";\r\n" + 
-			indentation + "}";
+			String getter = "\r\n" + "public " + member_type + " get" + member_identifier + "()" + "{\r\n" +
+			"\treturn " + member_identifier + ";\r\n" + 
+			"}";
+			String setter = "\r\n" + "public " + "void" + " set" + member_identifier + "(" + member_type + " " + member_identifier + ")" + "{\r\n" + 
+			"\tthis." + member_identifier + " = " + member_identifier +";\r\n" + 
+			"}";
 			getSetResultLines.add(getter);
 			getSetResultLines.add(setter);
 		}
