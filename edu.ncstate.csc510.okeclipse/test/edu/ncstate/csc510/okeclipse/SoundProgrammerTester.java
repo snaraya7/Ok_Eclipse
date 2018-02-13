193cec71502ca2cfe837fd47118bbc345a4b3e44
ksivasu <ksivasu@ncsu.edu>
2018-02-13 14:11:25 -0500

    SoundProgrammerTester

--- edu.ncstate.csc510.okeclipse/test/edu/ncstate/csc510/okeclipse/SoundProgrammerTester.java
+++ edu.ncstate.csc510.okeclipse/test/edu/ncstate/csc510/okeclipse/SoundProgrammerTester.java
@@ -1,24 +1,96 @@
-package edu.ncstate.csc510.okeclipse;
 
-import static org.junit.Assert.*;
+	//@Test
+	//public void testGenerateMainMethod() {
+	package edu.ncstate.csc510.okeclipse;
+	import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
+	import edu.ncstate.csc510.okeclipse.common.*;
+	import edu.ncstate.csc510.okeclipse.handlers.SoundProgrammerImpl;
 
-public class SoundProgrammerTester {
+	public class SoundProgrammerTester {
+
+//Checking for a change
+		@Test
+		public void testExtract() {
+			ISoundProgrammer soundprogram = new SoundProgrammerImpl();
+			String exp_output = new String("public class Apple {\r\n" +
+					"\r\n" +
+					"	private int seeds;\r\n" +
+					"	private String color;\r\n" +
+					"	\r\n" +
+					"	public int getSeeds() {\r\n" +
+					"		return seeds;\r\n" +
+					"	}\r\n" +
+					"	public void setSeeds(int seeds) {\r\n" +
+					"		this.seeds = seeds;\r\n" +
+					"	}\r\n" +
+					"	public String getColor() {\r\n" +
+					"		return color;\r\n" +
+					"	}\r\n" +
+					"	public void setColor(String color) {\r\n" +
+					"		this.color = color;\r\n" +
+					"	}\r\n" +
+					"\r\n" +
+					"}");
+			String output = soundprogram.generateGetterSetter("public class Apple {\r\n" +
+					"\r\n" +
+					"	private int seeds;\r\n" +
+					"	private String color;\r\n" +
+					"\r\n" +
+					"}\r\n" +
+					"");
+			List<String> bothList1= Arrays.asList(output.split("\\s+"));
+			List<String> bothList2= Arrays.asList(exp_output.split("\\s+"));
+
+			System.out.println(bothList1);
+				System.out.println(bothList2);
+				if (bothList1.equals(bothList2))
+					System.out.println("Test PASS");
+				else
+				System.out.println("Test FAIL");
+
+		}
 
 	@Test
-	public void testGenerateMainMethod() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGenerateGetterSetter() {
-		fail("Not yet implemented");
+	public void main() {
+		SoundProgrammerTester LCTest = new SoundProgrammerTester();
+		LCTest.testExtract();
+		LCTest.testImplementInterface();
 	}
 
 	@Test
 	public void testImplementInterface() {
-		fail("Not yet implemented");
-	}
+		ISoundProgrammer soundprogram = new SoundProgrammerImpl();
+		String exp_output = new String("public class Car implements Vehicle {\r\n" +
+				"	\r\n" +
+				"	public void start(){ }\r\n" +
+				"	\r\n" +
+				"	public boolean stop(){ return false; }\r\n" +
+				"\r\n" +
+				"}");
+		String output = soundprogram.implementInterface("public class Car {\r\n" +
+				"	\r\n" +
+				"\r\n" +
+				"}\r\n" +
+				"","public interface Vehicle {\r\n" +
+				"\r\n" +
+				"	public void start();\r\n" +
+				"	\r\n" +
+				"	public boolean stop();\r\n" +
+				"\r\n" +
+				"} ");
+		List<String> bothList1= Arrays.asList(output.split("\\s+"));
+		List<String> bothList2= Arrays.asList(exp_output.split("\\s+"));
+		System.out.println(bothList1);
+		System.out.println(bothList2);
+		if (bothList1.equals(bothList2))
+		{
+			System.out.println("Test Case Pass");
+		}
+		else
+			System.out.println("Test Case fail");
 
-}
+	}
+	}
