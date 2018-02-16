b9735bfc4f9ad3550e4ceba631f36f879762d55a
ksivasu <ksivasu@ncsu.edu>
2018-02-15 19:45:07 -0500

    Inject Code

--- edu.ncstate.csc510.okeclipse/src/edu/ncstate/csc510/okeclipse/handlers/SoundProgrammerImpl.java
+++ edu.ncstate.csc510.okeclipse/src/edu/ncstate/csc510/okeclipse/handlers/SoundProgrammerImpl.java
@@ -1,8 +1,18 @@
 package edu.ncstate.csc510.okeclipse.handlers;
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 
+import com.sohelper.datatypes.StackoverflowAnswer;
+
+import edu.ncstate.csc510.okeclipse.builder.SOAnswerBuilder;
 import edu.ncstate.csc510.okeclipse.common.ISoundProgrammer;
 import edu.ncstate.csc510.okeclipse.util.Util;
 
@@ -16,7 +26,11 @@ public class SoundProgrammerImpl implements ISoundProgrammer {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	public static void main(String[] args) {
+		
+		SoundProgrammerImpl Object = new SoundProgrammerImpl();
+		Object.injectCode("$How to write a for loop in java$ $How to write a while loop in java$");
+	}
 	/**
 	 * Refer getter_setter_requirement.txt in explanation package
 	 */
@@ -56,7 +70,8 @@ public class SoundProgrammerImpl implements ISoundProgrammer {
 
 
 	@Override
-	public void insertContent(String soCodeContent, int position) throws BadLocationException {
+	public void insertContent(String soCodeContent, int position) throws BadLocationException 
+	{
 
 		IDocument doc = Util.getCurrentEditorContent();
 
@@ -66,9 +81,97 @@ public class SoundProgrammerImpl implements ISoundProgrammer {
 
 	@Override
 	public void injectCode(String javaClass) {
-
-		System.out.println("injecting code for "+javaClass);
-		
-	}
-
+	        
+		int left = javaClass.indexOf("$");//String input = "test string (67)";
+		ArrayList<String> input = new ArrayList<String>();
+		while (left >= 0)
+    	{
+    	String sub1 = javaClass.substring(left+1);
+    	int right_pre = sub1.indexOf("$");
+    	//System.out.println(right_pre);
+    	int right = left + right_pre;
+    	//System.out.println(right);
+    	String sub = javaClass.substring(left+1, right+1);
+    	//System.out.println(right);
+    	//System.out.println(sub1);
+    	System.out.println(sub);
+    	System.out.println("\n");
+    	input.add(sub);
+    	//System.out.println("\n");
+	    javaClass = javaClass.substring(right+2);
+	    //System.out.println(javaClass);
+    	left = javaClass.indexOf("$");
+    	}
+		for (String question : input)
+    	{
+			System.out.println(question);
+    	}
+		//List<StackoverflowAnswer> stackoverflowAnswers = new List<StackoverflowAnswer>();
+		List<StackoverflowAnswer> myList = new ArrayList<StackoverflowAnswer>();
+		IProgressMonitor monitor = new NullProgressMonitor();
+		SOAnswerBuilder Ansfetcher = new SOAnswerBuilder();
+		try {
+        	for (String question : input)
+        	{
+        		System.out.println("Entered Loop");
+        		System.out.println(question);
+			myList = Ansfetcher.extractAnswers(question, monitor );
+        	
+		 
+        //System.out.println(myList);
+		//System.out.println("injecting code for "+javaClass);
+        for (StackoverflowAnswer answer : myList) {
+        	if (answer.isAccepted())
+        	{
+        		String Answer = answer.getBody();
+        		//System.out.println(Answer);
+        	left = Answer.indexOf("<code>");
+        	while (left > 0)
+        	{
+        	String sub1 = Answer.substring(left+6);
+        	int right_pre = sub1.indexOf("</code>");
+        	int right = left + right_pre;
+        	//System.out.println(right);
+        	String sub = Answer.substring(left+6, right-1);
+        	//System.out.println(sub1);
+        	System.out.println(sub);
+        	//System.out.println("\n");
+        	Answer = Answer.substring(right-1);
+        	left = Answer.indexOf("<code>");
+        	}
+        	}
+            }
+        	}}
+        	catch (IOException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+		}
 }
+        	//      
+        	
+//        	{
+//        		if (answer.isUpVoted())
+//        		{
+//        			String Answer = answer.getBody();
+//            		System.out.println(Answer);
+//            	int left = Answer.indexOf("<code>");
+//            	while (left > 0)
+//            	{
+//            	String sub1 = Answer.substring(left+6);
+//            	int right_pre = sub1.indexOf("</code>");
+//            	int right = left + right_pre;
+//            	//System.out.println(right);
+//            	String sub = Answer.substring(left+6, right-1);
+//            	//System.out.println(sub1);
+//            	System.out.println(sub);
+//            	//System.out.println("\n");
+//            	Answer = Answer.substring(right-1);
+//            	left = Answer.indexOf("<code>");
+//            	}
+//        		}
+//        	}
+
+
+	
+
